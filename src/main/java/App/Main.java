659cbbd29639e427e5de846ace301df2ee09c533
Misael Andres Maestre Acosta <mandresmaestre@unicesar.edu.co>
2026-08-14 15:30:32 -0500

    feat: implementar listClients de Cliente (listar y buscar)

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -11,44 +11,55 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    
+
     public static void createClient() {
-    
-    System.out.print("Enter client ID: ");
-    String id = sc.nextLine();
-    
-    System.out.print("Enter client name: ");
-    String name = sc.nextLine();
-    
-    System.out.print("Enter client phone: ");
-    String phone = sc.nextLine();
-    
-    System.out.print("Enter client email: ");
-    String email = sc.nextLine();
-    
-    
-    
-    boolean exists = false;
-    for (Client client : clients) {
-        if (client.getId().equals(id)) {
-            exists = true;
-            break;
+
+        System.out.print("Enter client ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Enter client name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter client phone: ");
+        String phone = sc.nextLine();
+
+        System.out.print("Enter client email: ");
+        String email = sc.nextLine();
+
+        boolean exists = false;
+        for (Client client : clients) {
+            if (client.getId().equals(id)) {
+                exists = true;
+                break;
+            }
+        }
+
+        if (exists) {
+            System.out.println("Client with ID " + id + " already exists");
+            return;
+        }
+
+        Client newClient = new Client(id, name, phone, email);
+        clients.add(newClient);
+
+        System.out.println("Client created successfully");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Phone: " + phone);
+        System.out.println("Email: " + email);
+
+    }
+
+    public static void listClients() {
+        if (clients.isEmpty()) {
+            System.out.println("No clients registered");
+            return;
+        }
+
+        System.out.println("\n CLIENTS LIST ");
+        for (Client client : clients) {
+            System.out.println(client);
         }
     }
-    
-    if (exists) {
-        System.out.println("Client with ID " + id + " already exists");
-        return;
-    }
-    
-    Client newClient = new Client(id, name, phone, email);
-    clients.add(newClient);
-    
-    System.out.println("Client created successfully");
-    System.out.println("ID: " + id);
-    System.out.println("Name: " + name);
-    System.out.println("Phone: " + phone);
-    System.out.println("Email: " + email);
-    
-}
+
 }
