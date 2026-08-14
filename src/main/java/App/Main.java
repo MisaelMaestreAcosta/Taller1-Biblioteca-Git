354387095fa2ab0ee39dbe161e4bb1f801260ce2
Misael Andres Maestre Acosta <mandresmaestre@unicesar.edu.co>
2026-08-14 11:24:50 -0500

    feat: Crear clase Persona como padre de Cliente

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package App;
 
-/**
- *
- * @author y
- */
+
 public class Main {
 
     public static void main(String[] args) {
