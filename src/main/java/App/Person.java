88890679b51575d23f1f7e38d0addba242caab45
Misael Andres Maestre Acosta <mandresmaestre@unicesar.edu.co>
2026-08-15 10:34:58 -0500

    feat: crear clase Material como padre de Libro

--- src/main/java/App/Person.java
+++ src/main/java/App/Person.java
@@ -1,25 +1,23 @@
 package App;
 
 public class Person {
-<<<<<<< HEAD
+
 
     private String id;
     private String name;
     private String phone;
 
-=======
+
     
-    private String id;
-    private String name;
-    private String phone;
+ 
     
->>>>>>> develop
+
     public Person(String id, String name, String phone) {
         this.id = id;
         this.name = name;
         this.phone = phone;
     }
-<<<<<<< HEAD
+
 
     public String getId() {
         return id;
@@ -50,21 +48,5 @@ public class Person {
         return "Person{id=" + id + ", name=" + name + ", phone=" + phone + "}";
 
     }
-=======
-    
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
-    
-    @Override
-    public String toString() {
-        return "Person{id=" + id + ", name=" + name + ", phone=" + phone + "}";
-    }
-    
->>>>>>> develop
+
 }
